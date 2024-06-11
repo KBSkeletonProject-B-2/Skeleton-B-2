3a7b89bf0afb3fe475cec6f0d5023c6a7dcad69d
JeongHyunBaek <bjhbhm@gmail.com>
2024-06-11 13:10:07 +0900

    update TransInfoCreate.vue, add json

--- src/pages/TransInfoCreate.vue
+++ src/pages/TransInfoCreate.vue
@@ -1,11 +1,117 @@
+<!-- 거래내역 저장 뷰 -->
+<!-- 가계부 내역을 생성하고 수정하는 뷰이다. -->
 <template>
     <div>
-
+        <form action="/action_page.php" @submit.prevent="createFormSubmitHandler">
+            <div class="mb-3 mt-3">
+                <label for="date" class="form-label transinfocreate-color">날짜</label>
+                <input type="date" class="form-control transinfocreate-input" id="date" required
+                    v-model="transInfo.date">
+            </div>
+            <div class="mb-3">
+                <label for="category" class="form-label transinfocreate-color">카테고리</label>
+                <select class="form-select transinfocreate-select" id="category" required v-model="transInfo.category">
+                    <option value="" selected disabled hidden>선택</option>
+                    <option v-for="c in cList" :value="c.eng">{{ c.kor }}</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="amount" class="form-label transinfocreate-color">금액</label>
+                <input type="text" class="form-control transinfocreate-input" id="amount" required
+                    v-model="transInfo.amount">
+            </div>
+            <div class="mb-3">
+                <label for="memo" class="form-label transinfocreate-color">메모</label>
+                <input type="text" class="form-control transinfocreate-input" id="memo" v-model="transInfo.memo">
+            </div>
+            <button @click="clickButtonHandler" type="button" class="btn transinfocreate-cancel">취소</button>
+            <button type="submit" class="btn transinfocreate-save">저장</button>
+        </form>
     </div>
 </template>
 
-<script setup>
+<script>
+import axios from 'axios'
+import { onMounted, reactive } from 'vue';
+export default {
+    setup() {
+        let cList = reactive([])
+        let transInfo = reactive({
+            id: Date.now(),
+            date: "",
+            category: "",
+            amount: "",
+            memo: ""
+        })
 
+        onMounted(async () => {
+            try {
+                const url = "http://localhost:3001/category"
+                const response = await axios.get(url)
+
+                Object.assign(cList, response.data)
+
+                console.log(response.data)
+            } catch (err) {
+                console.log(err.message)
+                alert("카테고리 조회 실패")
+            }
+        })
+
+        const createFormSubmitHandler = async () => {
+            try {
+                if (document.getElementById("category").value === "") {
+                    alert("카테고리를 선택해주세요.")
+                } else {
+                    const url = "http://localhost:3001/transInfo"
+                    const response = await axios.post(url, transInfo)
+
+                    document.getElementById("date").value = ""
+                    document.getElementById("category").value = ""
+                    document.getElementById("amount").value = ""
+                    document.getElementById("memo").value = ""
+
+                    console.log(response.data)
+                }
+            } catch (err) {
+                console.log(err.message)
+                alert("가계부 저장 실패")
+            }
+        }
+
+        const clickButtonHandler = () => {
+            document.getElementById("date").value = ""
+            document.getElementById("category").value = ""
+            document.getElementById("amount").value = ""
+            document.getElementById("memo").value = ""
+        }
+        return { cList, transInfo, createFormSubmitHandler, clickButtonHandler }
+    }
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style>
+.transinfocreate-color {
+    color: #84B9C0;
+    font-weight: bold;
+}
+
+.transinfocreate-cancel {
+    color: #C0C0C0;
+    font-weight: bold;
+    float: left;
+}
+
+.transinfocreate-input {
+    background-color: #D9D9D9;
+}
+
+.transinfocreate-select {
+    background-color: #D9D9D9;
+}
+
+.transinfocreate-save {
+    color: #84B9C0;
+    float: right;
+}
+</style>
