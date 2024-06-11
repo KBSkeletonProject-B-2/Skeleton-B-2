371a238963649ca46237515c4994006d070a5ad1
JeongHyunBaek <69346978+JeongHyunBaek@users.noreply.github.com>
2024-06-11 09:47:33 +0900

    add TransInfoCreate.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
-
+    <Header />
+    <router-view to="transinfocreate"></router-view>
+    <Footer />
   </div>
 </template>
 
 <script setup>
-
+import Footer from './components/Footer.vue';
+import Header from './components/Header.vue';
 </script>
 
 <style lang="scss" scoped></style>
