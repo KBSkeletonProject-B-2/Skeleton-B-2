b0b6c70c09e6a4695a42e81954031a0c88792437
yonggwonk <gwonluck@dgu.ac.kr>
2024-06-11 13:45:05 +0900

    Update FilterBar.vue
    Json 서버로부터 'category' 데이터 불러와 드롭다운 리스트에 표시

--- src/pages/FilterBar.vue
+++ src/pages/FilterBar.vue
@@ -3,45 +3,62 @@
     시작일, 종료일, 카테고리, 내용을 전달하는 조회하는 뷰이다. -->
 <template>
     <form @submit.prevent="send">
-      <label>
-        시작일<br />
-        <input type="date" v-model="startDate" />
-      </label>
-      <label>
-        종료일<br />
-        <input type="date" v-model="endDate" />
-      </label>
-      <label>
-        카테고리<br />
-        <select v-model="category">
-          <option value="">선택</option>
-          <option value="category1">카테고리 1</option>
-          <option value="category2">카테고리 2</option>
-
-        </select>
-      </label>
-      <label>
-        내용<br />
-        <input type="text" v-model="word" />
-      </label>
-      <button type="submit">조회</button>
+        <label>
+            시작일<br/>
+            <input type="date" v-model="startDate" />
+        </label>
+        <label>
+            종료일<br/>
+            <input type="date" v-model="endDate" />
+        </label>
+        <label>
+            카테고리<br/>
+            <select v-model="selectedCategory">
+                <option value="">선택</option>
+                <option v-for="c in category">{{ c.kor }}</option>
+            </select>
+        </label>
+        <label>
+            내용<br/>
+            <input type="text" v-model="title" />
+        </label>
+        <button type="submit">조회</button>
     </form>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue';
-  
-  const emit = defineEmits(['search']);
-  const startDate = ref('');
-  const endDate = ref('');
-  const category = ref('');
-  const word = ref('');
-  
-  const send = () => {
-    emit('search', startDate.value, endDate.value, category.value, word.value);
-  };
-  </script>
-  
-  <style scoped>
+</template>
 
-  </style>
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const emit = defineEmits(['search']);
+const startDate = ref('');
+const endDate = ref('');
+const selectedCategory = ref('');
+const title = ref('');
+const category = ref([]);
+
+/**
+ * 카테고리 목록 마운트
+ * 
+ * json 서버로부터 카테고리 목록을 가져온다.
+ */
+onMounted(async () => {
+    try {
+        const response = await axios.get("http://localhost:3000/category");
+        category.value = response.data;
+    } catch (error) {
+        console.error('Failed to fetch categories:', error);
+    }
+});
+
+/**
+ * 조회 이벤트 전송
+ * 
+ * 상위 컴포넌트인 Transactions.vue로 조회에 쓰일 조건 값 전달
+ */
+const send = () => {
+    emit('search', startDate.value, endDate.value, selectedCategory.value, title.value);
+};
+</script>
+
+<style scoped></style>
